d182b6eff0600a41a4490edb73d3b085aa5438ca
Gabriel Morais <gabriel.m.g.morais@gmail.com>
2023-06-13 22:55:24 -0300

    add notations and enable auto drop in JPA configuration

--- src/main/kotlin/edu/dio/bootcampTQI/credit/application/system/model/Address.kt
+++ src/main/kotlin/edu/dio/bootcampTQI/credit/application/system/model/Address.kt
@@ -1,6 +1,11 @@
 package edu.dio.bootcampTQI.credit.application.system.model
 
+import jakarta.persistence.*
+
+@Embeddable
 data class Address(
+    @Column(nullable = false)
     var zipCode: String = "",
+    @Column(nullable = false)
     var street: String = ""
 )
--- src/main/kotlin/edu/dio/bootcampTQI/credit/application/system/model/Credit.kt
+++ src/main/kotlin/edu/dio/bootcampTQI/credit/application/system/model/Credit.kt
@@ -1,16 +1,26 @@
 package edu.dio.bootcampTQI.credit.application.system.model
 
 import edu.dio.bootcampTQI.credit.application.system.enummeration.Status
-import java.util.*
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.util.*
 
+@Entity
+@Table(name = "Credit")
 data class Credit(
+    @Column(nullable = false, unique = true)
     val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false)
     val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false)
     val dayFirstInstallment: LocalDate,
+    @Column(nullable = false)
     val numberOfInstallments: Int = 0,
+    @Enumerated
     val status: Status = Status.IN_PROGRESS,
-    val custumer: Custumer? = null,
+    @ManyToOne
+    val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val idCredit: Long? = null
 )
